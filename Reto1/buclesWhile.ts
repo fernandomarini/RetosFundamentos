41ab734ddf97b8850d147808a83f00cd39f00cca
Fernando Marini <fer.ms114@gmail.com>
2023-02-13 09:53:10 -0300

    condicionalesCorreccionFinal.02

--- Reto1/buclesWhile.ts
+++ Reto1/buclesWhile.ts
@@ -4,18 +4,21 @@
  introduces como parámetro de entrada.
  La cabecera de la función tendrá el siguiente aspecto: function hasEven(myNums) */
 
- let array = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20];
+ let array = [1,3,5,7,9,10,11,12,13,14,15,16,17,18,19,20];
 
  function hasEven(myNums:number[]){
     let i = 0;
-    let n:number = 0;
-    while( i < array.length && n % 2 == 0 ){            
-        return "Existe un numero par."
-        i++;
+    while( i < array.length && myNums[i] % 2 == 1 ){          
+       i++;
     };
+    if( i != myNums.length){
+        return "Existe"
+    } else {
+        return "No Existe"
+    }
 };
 
- console.log(hasEven(array));
+ console.log(`En el array ${hasEven(array)} un numero par`);
 
 /* 
  Realizar una función que reciba un array de nombres y te devuelva verdadero si y solo si
@@ -27,22 +30,16 @@ let nombres2 :string[] =["ramiro","fernando","ariel"];
 
 function startWithM ( myNames:string[] ){
     let i = 0;
-    while( i < myNames.length && myNames[i][i] == "m" )
-   return true;
-    i++
+    while( i < myNames.length && myNames[i][i] != "m" ){    
+    i++    
+    };
+    if(i != myNames.length ){
+        return "comienzan"
+    } else {
+        return "No comienzan"
+    }
 };
 
 
-console.log(`Los nombres comienzan con M, es ${startWithM(nombres1)}`);
-
-/* 
-let i = 0;
-do{
-    if( myNames[i][0] != "m" ){
-        break;            
-    };         
-    if( ( myNames.length -1 ) == i ){ 
-        console.log(`VERDADERO, todos los nombres comienzan con M`); 
-    };
-    i++;        
-}while( i < myNames.length && myNames[i][0] == "m") */
+console.log(`Los nombresde array ${startWithM(nombres1)} con la letra m`);
+console.log(`Los nombresde array ${startWithM(nombres2)} con la letra m`);
